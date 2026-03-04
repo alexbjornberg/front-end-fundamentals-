d443568623f517809df724760729a7cb738f3db6
alexbjornberg <alexbjornberg@icloud.com>
2026-03-04 11:59:28 +0100

    made a functioning cart that can add and remove items

---
+++ js/cart.js
@@ -0,0 +1,16 @@
+const cartGrid = document.querySelector(".cart-grid");
+let cartArr = localStorage.cart ? JSON.parse(localStorage.cart) : [];
+
+if (!cartArr || cartArr.length === 0) {
+  cartGrid.innerHTML = "<p>The cart is empty.</p>";
+} else {
+  cartGrid.innerHTML = "";
+  for (let i = 0; i < cartArr.length; i++) {
+    const cartElement = document.createElement("img");
+    cartElement.src = cartArr[i].img;
+    cartElement.style.width = "60%";
+    cartGrid.appendChild(cartElement);
+  }
+}
+
+/* AI was used on this page to help with debugging and helping me a little with some line of code */
---
+++ js/script.js
@@ -0,0 +1,43 @@
+ const cartButtons = document.getElementsByClassName("addtocart");
+
+let cartArr = localStorage.cart ? JSON.parse(localStorage.cart) : [];
+
+
+for (let i = 0; i < cartButtons.length; i++) {
+  for (let j = 0; j < cartArr.length; j++) {
+    if (cartButtons[i].dataset.id === cartArr[j].id) {
+      cartButtons[i].src = "img/removefromcart.png";
+    }
+  }
+}
+
+
+for (let i = 0; i < cartButtons.length; i++) {
+  cartButtons[i].addEventListener("click", saveToCart);
+}
+
+function saveToCart(e) {
+  
+  e.preventDefault();
+
+  const item = {
+    id: this.dataset.id,
+    img: this.dataset.img,
+  };
+
+
+  const index = cartArr.findIndex((p) => p.id === item.id);
+
+  if (index !== -1) {
+    cartArr.splice(index, 1);
+    this.src = "img/addtocart.png";
+  } else {
+    cartArr.push(item);
+    this.src = "img/removefromcart.png";
+  }
+
+  localStorage.cart = JSON.stringify(cartArr);
+}
+/*  
+AI was used on this page to help with debugging and helping me a little with some line of code
+ */
